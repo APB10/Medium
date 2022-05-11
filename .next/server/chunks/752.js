4e6e61f404873c8770b9319ce8c5be66023eb2e4
Ali P. Bouraki <ali_panahi10@hotmail.com>
2022-05-11 16:57:02 +0100

    adding next troubleshooting vercel deployment

---
+++ .next/server/chunks/752.js
@@ -0,0 +1,102 @@
+"use strict";
+exports.id = 752;
+exports.ids = [752];
+exports.modules = {
+
+/***/ 505:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_Link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(514);
+/* harmony import */ var next_Link__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_Link__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+function Header() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("header", {
+        className: "mx-auto flex max-w-7xl justify-between p-5",
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "flex items-center space-x-5",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_Link__WEBPACK_IMPORTED_MODULE_2___default()), {
+                        href: "/",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                            className: "w-44 cursor-pointer object-contain",
+                            src: "https://links.papareact.com/yvf",
+                            alt: ""
+                        })
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "hidden items-center space-x-5 md:inline-flex",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                children: "About"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                children: "Contact"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                                className: "text-whtie rounded-full bg-green-600 px-4 py-1",
+                                children: "Follow"
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "flex items-center space-x-5 text-green-600",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                        children: " Sign In"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h3", {
+                        className: "rounded-full border border-green-600 px-4 py-1",
+                        children: "Get Started"
+                    })
+                ]
+            })
+        ]
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Header);
+
+
+/***/ }),
+
+/***/ 994:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "i3": () => (/* binding */ sanityClient),
+/* harmony export */   "uH": () => (/* binding */ urlFor)
+/* harmony export */ });
+/* unused harmony exports config, useCurrentUser */
+/* harmony import */ var _sanity_image_url__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(791);
+/* harmony import */ var _sanity_image_url__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_sanity_image_url__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_sanity__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(879);
+/* harmony import */ var next_sanity__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_sanity__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const config = {
+    dataset: "production" || 0,
+    projectId: "e36qdxjn",
+    apiVersion: "2021-03-25",
+    useCdn: "production" === "production"
+};
+const sanityClient = (0,next_sanity__WEBPACK_IMPORTED_MODULE_1__.createClient)(config);
+const urlFor = (source)=>_sanity_image_url__WEBPACK_IMPORTED_MODULE_0___default()(config).image(source)
+;
+const useCurrentUser = (0,next_sanity__WEBPACK_IMPORTED_MODULE_1__.createCurrentUserHook)(config);
+
+
+/***/ })
+
+};
+;
